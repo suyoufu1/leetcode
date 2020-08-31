aa2d780e4e244fd1aedf4a4176aafc2ab9ec3491
syf <suyfu@yonyou.com>
2020-08-31 19:45:24 +0800

    两链表之和

---
+++ src/main/java/com/syf/leetcode/arrayType/LongestSubString.java
@@ -0,0 +1,38 @@
+package com.syf.leetcode.arrayType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 找到一个最长子字符串的长度而不重复的字符串
+ *  例如：
+ *      abcabcabb -> abc -> 3
+ *      bbbbb -> b -> 1
+ *      pwwkew -> wke -> 3
+ *  思想：
+ *      窗口滑动方法[i,j)
+ */
+public class LongestSubString {
+    public static void main(String[] args) {
+        int max = maxL("abcabcabb");
+        System.out.println(max);
+    }
+    public static int maxL(String str){
+        int max = 0 ;
+        //i表示开始，j表示结尾
+        int i = 0 , j = 0;
+        Set<Character> set = new HashSet<>();
+        while ( i < str.length() && j < str.length()){
+            if(!set.contains(str.charAt(j))){
+                set.add(str.charAt(j));
+                max = Math.max(max,j - i + 1);
+                j ++ ;
+            }else {
+                set.remove(str.charAt(i));
+                i ++ ;
+            }
+        }
+        return max;
+    }
+
+}
